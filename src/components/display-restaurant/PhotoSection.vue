a9fb3332d97591de0e7c7a90d5b134945921b544
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2020-03-13 21:40:20 +0800

    Changed image rendering method

--- src/components/display-restaurant/PhotoSection.vue
+++ src/components/display-restaurant/PhotoSection.vue
@@ -3,8 +3,7 @@
         <div class="carousel-container">
           <h3>Golden Fortune {{title}}</h3>
           <div class="carousel carousel-slider">
-            <a class="carousel-item" href=""><img src="../../assets/pictures/golden-menu.jpg"></a>
-            <a class="carousel-item" href=""><img src="../../assets/pictures/Golden Fortune-1.jpg"></a>
+            <a class="carousel-item" href="" v-for="url in urls" :key="url.url"><img :src="url.url"></a>
           </div>
         </div>
     </div>
@@ -16,7 +15,15 @@ export default {
     name: "PhotoSection",
     props: {
       title: String //Title of Photo section (Menu / Photos)
-    }, 
+    },
+    data() {
+      return {
+        urls: [
+          {url: require("../../assets/pictures/golden-menu.jpg")},
+          {url: require("../../assets/pictures/Golden Fortune-1.jpg")}
+        ]
+      };
+    },
     mounted() {
       M.AutoInit(); 
     }
