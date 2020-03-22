5515c78679b91ce6a06444be87e5412ae157c1e6
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2020-03-22 14:17:17 +0800

    Consistency changes for models

--- server/scripts/populateUsers.js
+++ server/scripts/populateUsers.js
@@ -33,7 +33,6 @@ async function populateUsers(userCounter, limit) {
 
     for(i = userCounter; i < limit; i++) {
         let user = new User({
-            userID: mongoose.Types.ObjectId(),
             name: faker.name.firstName() + " " + faker.name.lastName(),
             password: faker.lorem.words(1),
             email: faker.internet.email(),
@@ -47,34 +46,4 @@ async function populateUsers(userCounter, limit) {
     return;
 } 
 
-function connectUserReviews() {
-    User.find({}, (err, users) => {})
-    .then((res) => {
-        res.forEach((user) => {
-            //Get all reviews associated with user 
-            Review.find({ownerID : user.userID}, "restaurantID")
-                .then(review => {
-                    let restoIDs = []; 
-                    //Get the resto IDs and place into an array
-                    review.forEach((resto, beenHere) => { 
-                        beenHere.push(resto.restaurantID); 
-                    });
-                    User.update({userID: user.userID}, {beenHere : restoIDs, reviewed : restoIDs}, () => {
-                        console.log("Updated"); 
-                    })
-                })
-            //Randomize the reviews liked by the user 
-            
-        })
-        //Get all the reviews then for each user
-        //Search the review with their objectID then assign resto to been here, reviewed, 
-        //then randomize a review to like increment the review's upvote counter 
-    })
-    
-}
-
-function updateUserPoints() {
-    //update the user's no. of points based on their reviews 
-}
-
-module.exports = {populateUsers, connectUserReviews, updateUserPoints }; 
+module.exports = populateUsers; 
