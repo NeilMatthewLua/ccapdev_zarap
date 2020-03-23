224788384651100a5153246facfb15dfb4db5ca0
unknown <neil_lua@dlsu.edu.ph>
2020-03-23 14:34:49 +0800

    Register working

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -7,8 +7,7 @@ const User = require('../models/users.js');
 const Picture = require('../models/pictures.js');
 const Restaurant = require('../models/restaurants.js');
 
-router.post('/addUser', async (req, res, next) => { //adds a user
-
+router.post('/addUser',async (req, res, next) => { //adds a user
     let pictureID
     await Picture.find({}, {}, { sort: { '_id' : -1 } },  function(err, post) {
         pictureID = post[0]['pictureID']
@@ -30,15 +29,15 @@ router.post('/addUser', async (req, res, next) => { //adds a user
         .save()
         .then(result => {
             console.log(result);
+            res.status(200).send({auth: true})
         })
         .catch(err => {
-            console.log(err);
+            res.status(500).send("There was a problem with registering the user")
         });
 })
 
 router.get('/:userID', (req, res, next) => { //finds a user by userID
     const id = req.params.userID;
-    console.log(id);
     User.find({userID: id})
         .exec()
         .then(doc => {
@@ -51,7 +50,6 @@ router.get('/:userID', (req, res, next) => { //finds a user by userID
 })
 
 router.get('/', (req, res, next) => { //finds a user by userID
-    console.log("All users:");
     User.find({})
         .exec()
         .then(doc => {
@@ -61,15 +59,28 @@ router.get('/', (req, res, next) => { //finds a user by userID
         .catch(err => {
             res.send(500).json({error: err});
         })
-    Restaurant.find({})
-        .exec()
-        .then(doc => {
-            console.log(doc);
-            res.status(200).json(doc);
-        })
-        .catch(err => {
-            res.send(500).json({error: err});
-        })
+})
+
+router.post('/login', (req, res) => {
+
+    User.find({email: req.body.email})
+    .then(user => {
+
+    })
+    .catch(err => {
+        return res.status(500).send('Error on the server.');
+    })
+    
+    // (req.body.email, (err, user) => {
+    //     if (err) return res.status(500).send('Error on the server.');
+    //     if (!user) return res.status(404).send('No user found.');
+    //     let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    //     if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+    //     let token = jwt.sign({ id: user.id }, config.secret, {
+    //         expiresIn: 86400 // expires in 24 hours
+    //     });
+    //     res.status(200).send({ auth: true, token: token, user: user });
+    // });
 })
 
 module.exports = router;
